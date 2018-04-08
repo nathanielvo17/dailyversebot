1259d1046dd9031d31e72a8a0d8cbe10ed8c1f44
nathanielvo17 <37366193+nathanielvo17@users.noreply.github.com>
2018-04-08 03:50:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests, json, os, urllib.request
 
-with urllib.request.urlopen("http://labs.bible.org/api/?passage=votd&type=json") as url:
+with urllib.request.urlopen("http://labs.bible.org/api/?passage=random&type=json") as url:
     data = json.loads(url.read().decode())
 print(data)
     
@@ -9,8 +9,13 @@ chapter = data[0]["chapter"]
 verse = data[0]["verse"]
 text = data[0]["text"]
 
+if len(data) > 1:
+    text = "%s %s" % (verse, text)
+    verse += '-' + data[len(data)-1]["verse"]
+    for i in range(1, len(data)):
+        text += " %s %s" % (data[i]["verse"], data[i]["text"])
 
-post = book + " " + chapter + ":" + verse + " " + '\n' + text
+post = "%s %s:%s ~ \n \t %s" % (book, chapter, verse, text)
 output = { 'bot_id' : os.getenv('GROUPME_BOT_ID'),
          'text': post }
 requests.post('https://api.groupme.com/v3/bots/post',
